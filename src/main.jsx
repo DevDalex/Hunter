70f1174e06060a6f456cbf3d4356875a9ff7fe9a
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-07-20 00:44:36 +0300

    Add console log deployment test

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import './styles.css';
 import './redesign.css';
 import './v3.css';
 
+console.log('test 123');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SiteErrorBoundary><App /></SiteErrorBoundary>
