3a918cba820f33c8af814db0fe3aaa6cfc757840
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-07-24 23:41:03 +0300

    Merge expanded character state coverage

--- src/data/succession/entitiesCharacterFoundation.js
+++ src/data/succession/entitiesCharacterFoundation.js
@@ -1,7 +1,23 @@
 import { successionArchiveData as relationshipFoundationData } from './entitiesRelationshipFoundation.js';
 import { characterStateProfiles } from './characterStateFoundation.js';
+import { characterStateExpansionProfiles } from './characterStateExpansion.js';
+
+const characterIds = new Set([
+  ...Object.keys(characterStateProfiles),
+  ...Object.keys(characterStateExpansionProfiles),
+]);
+
+const mergedCharacterStateProfiles = Object.freeze(Object.fromEntries(
+  [...characterIds].map((characterId) => [
+    characterId,
+    Object.freeze([
+      ...(characterStateProfiles[characterId] || []),
+      ...(characterStateExpansionProfiles[characterId] || []),
+    ].sort((left, right) => left.chapterRange.start - right.chapterRange.start || left.id.localeCompare(right.id))),
+  ]),
+));
 
 export const successionArchiveData = Object.freeze({
   ...relationshipFoundationData,
-  characterStateProfiles,
+  characterStateProfiles: mergedCharacterStateProfiles,
 });
