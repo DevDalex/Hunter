270b00cced054ec15d414d1afa9e12bfafa65a0a
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-07-24 23:59:27 +0300

    Merge complete royal household state coverage

--- src/data/succession/entitiesCharacterFoundation.js
+++ src/data/succession/entitiesCharacterFoundation.js
@@ -1,10 +1,12 @@
 import { successionArchiveData as relationshipFoundationData } from './entitiesRelationshipFoundation.js';
 import { characterStateProfiles } from './characterStateFoundation.js';
 import { characterStateExpansionProfiles } from './characterStateExpansion.js';
+import { characterStateRoyalExpansionProfiles } from './characterStateRoyalExpansion.js';
 
 const characterIds = new Set([
   ...Object.keys(characterStateProfiles),
   ...Object.keys(characterStateExpansionProfiles),
+  ...Object.keys(characterStateRoyalExpansionProfiles),
 ]);
 
 const mergedCharacterStateProfiles = Object.freeze(Object.fromEntries(
@@ -13,6 +15,7 @@ const mergedCharacterStateProfiles = Object.freeze(Object.fromEntries(
     Object.freeze([
       ...(characterStateProfiles[characterId] || []),
       ...(characterStateExpansionProfiles[characterId] || []),
+      ...(characterStateRoyalExpansionProfiles[characterId] || []),
     ].sort((left, right) => left.chapterRange.start - right.chapterRange.start || left.id.localeCompare(right.id))),
   ]),
 ));
