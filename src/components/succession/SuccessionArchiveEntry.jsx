999c5143c50694211c1e1590dd0ee4fca90990d4
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-07-29 15:06:38 +0300

    Load Black Whale Succession theme

--- src/components/succession/SuccessionArchiveEntry.jsx
+++ src/components/succession/SuccessionArchiveEntry.jsx
@@ -2,5 +2,6 @@ import '../../styles/succession-archive.css';
 import './SuccessionArchiveContrast.css';
 import './SuccessionArchiveLayoutFixes.css';
 import './SuccessionArchiveCatalog.css';
+import './SuccessionBlackWhaleTheme.css';
 
 export { default } from './SuccessionArchiveApp';
