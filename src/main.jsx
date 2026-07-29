39e0117db0c31dc9f178ba667a4b94e291386c01
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-07-29 08:58:36 +0300

    Install final Nen focus ordering before render

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './data/normalizeNenSpectrumFocusOrder';
 import App from './App';
 import SiteErrorBoundary from './components/SiteErrorBoundary';
 import { installAccessibilityRuntime } from './lib/accessibilityRuntime';
