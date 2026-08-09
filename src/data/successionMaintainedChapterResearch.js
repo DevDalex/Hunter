d2d0d5dc022be527927d81c97748079c0eca0364
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-08-09 20:50:22 +0300

    Integrate Chapter 391 maintained research

--- src/data/successionMaintainedChapterResearch.js
+++ src/data/successionMaintainedChapterResearch.js
@@ -43,6 +43,7 @@ import { succession387ChapterResearch } from './succession387Research.js';
 import { succession388ChapterResearch } from './succession388Research.js';
 import { succession389ChapterResearch } from './succession389Research.js';
 import { succession390ChapterResearch } from './succession390Research.js';
+import { succession391ChapterResearch } from './succession391Research.js';
 import { succession400ChapterResearch } from './succession400Research.js';
 import { succession406ChapterResearch } from './succession406Research.js';
 import { succession408ChapterResearch } from './succession408Research.js';
@@ -97,6 +98,7 @@ export const maintainedSuccessionChapterResearch = Object.freeze([
   ...succession388ChapterResearch,
   ...succession389ChapterResearch,
   ...succession390ChapterResearch,
+  ...succession391ChapterResearch,
   ...succession400ChapterResearch,
   ...succession406ChapterResearch,
   ...succession408ChapterResearch,
