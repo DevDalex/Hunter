7e2e815ed49e62c4ae2557111ee9cba707a27e3b
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-08-09 18:39:29 +0300

    Integrate Chapter 389 events

--- src/data/succession/entitiesEventFoundation.js
+++ src/data/succession/entitiesEventFoundation.js
@@ -19,6 +19,7 @@ import { eventFoundation385Expansion } from './eventFoundation385Expansion.js';
 import { eventFoundation386Expansion } from './eventFoundation386Expansion.js';
 import { eventFoundation387Expansion } from './eventFoundation387Expansion.js';
 import { eventFoundation388Expansion } from './eventFoundation388Expansion.js';
+import { eventFoundation389Expansion } from './eventFoundation389Expansion.js';
 
 const ARCHIVE_DATE = '2026-08-09';
 const unique = (values) => [...new Set(values.filter(Boolean))];
@@ -45,6 +46,7 @@ const eventExpansions = Object.freeze([
   ...eventFoundation386Expansion,
   ...eventFoundation387Expansion,
   ...eventFoundation388Expansion,
+  ...eventFoundation389Expansion,
 ]);
 
 const events = Object.freeze(uniqueById([
