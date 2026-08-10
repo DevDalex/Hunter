d81ca644fb04f1c6b453adc32e44af1e6cb3c08c
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-08-10 06:24:46 +0300

    Register maintained Chapter 397 research

--- src/data/successionMaintainedChapterResearch.js
+++ src/data/successionMaintainedChapterResearch.js
@@ -49,6 +49,7 @@ import { succession393ChapterResearch } from './succession393Research.js';
 import { succession394ChapterResearch } from './succession394Research.js';
 import { succession395ChapterResearch } from './succession395Research.js';
 import { succession396ChapterResearch } from './succession396Research.js';
+import { succession397ChapterResearch } from './succession397Research.js';
 import { succession400ChapterResearch } from './succession400Research.js';
 import { succession406ChapterResearch } from './succession406Research.js';
 import { succession408ChapterResearch } from './succession408Research.js';
@@ -109,6 +110,7 @@ export const maintainedSuccessionChapterResearch = Object.freeze([
   ...succession394ChapterResearch,
   ...succession395ChapterResearch,
   ...succession396ChapterResearch,
+  ...succession397ChapterResearch,
   ...succession400ChapterResearch,
   ...succession406ChapterResearch,
   ...succession408ChapterResearch,
