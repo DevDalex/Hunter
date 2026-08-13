b838de51edcf7049a2d9bbd6aec0e9717f864d4e
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-08-13 18:54:29 +0300

    Extend relationship bridge

--- src/data/succession/relationshipFoundation412Expansion.js
+++ src/data/succession/relationshipFoundation412Expansion.js
@@ -1,7 +1,9 @@
 import { relationshipFoundation412ExpansionBase } from './relationshipFoundation412ExpansionBase.js';
 import { relationshipFoundation413Expansion } from './relationshipFoundation413Expansion.js';
+import { relationshipFoundation414Expansion } from './relationshipFoundation414Expansion.js';
 
 export const relationshipFoundation412Expansion = Object.freeze([
   ...relationshipFoundation412ExpansionBase,
   ...relationshipFoundation413Expansion,
+  ...relationshipFoundation414Expansion,
 ]);
