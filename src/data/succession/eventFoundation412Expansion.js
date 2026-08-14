7cd0e76b6ea50773c6dda1c5449df7a93843be8d
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-08-14 15:25:02 +0300

    Aggregate Chapter 415 events

--- src/data/succession/eventFoundation412Expansion.js
+++ src/data/succession/eventFoundation412Expansion.js
@@ -1,9 +1,11 @@
 import { eventFoundation412ExpansionBase } from './eventFoundation412ExpansionBase.js';
 import { eventFoundation413Expansion } from './eventFoundation413Expansion.js';
 import { eventFoundation414Expansion } from './eventFoundation414Expansion.js';
+import { eventFoundation415Expansion } from './eventFoundation415Expansion.js';
 
 export const eventFoundation412Expansion = Object.freeze([
   ...eventFoundation412ExpansionBase,
   ...eventFoundation413Expansion,
   ...eventFoundation414Expansion,
+  ...eventFoundation415Expansion,
 ]);
