1bafe57a31f7cea827991c3af89ea43896d5db8e
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-08-14 16:20:40 +0300

    Wire Chapter 416 relationships into canonical aggregate

--- src/data/succession/relationshipFoundation412Expansion.js
+++ src/data/succession/relationshipFoundation412Expansion.js
@@ -2,10 +2,12 @@ import { relationshipFoundation412ExpansionBase } from './relationshipFoundation
 import { relationshipFoundation413Expansion } from './relationshipFoundation413Expansion.js';
 import { relationshipFoundation414Expansion } from './relationshipFoundation414Expansion.js';
 import { relationshipFoundation415Expansion } from './relationshipFoundation415Expansion.js';
+import { relationshipFoundation416Expansion } from './relationshipFoundation416Expansion.js';
 
 export const relationshipFoundation412Expansion = Object.freeze([
   ...relationshipFoundation412ExpansionBase,
   ...relationshipFoundation413Expansion,
   ...relationshipFoundation414Expansion,
   ...relationshipFoundation415Expansion,
+  ...relationshipFoundation416Expansion,
 ]);
