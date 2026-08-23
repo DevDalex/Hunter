f982c8dae6be184286c5725653399662ecaee844
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-08-23 23:02:52 +0300

    Include Chapter 418 relationship foundation

--- src/data/succession/relationshipFoundation412Expansion.js
+++ src/data/succession/relationshipFoundation412Expansion.js
@@ -4,6 +4,7 @@ import { relationshipFoundation414Expansion } from './relationshipFoundation414E
 import { relationshipFoundation415Expansion } from './relationshipFoundation415Expansion.js';
 import { relationshipFoundation416Expansion } from './relationshipFoundation416Expansion.js';
 import { relationshipFoundation417Expansion } from './relationshipFoundation417Expansion.js';
+import { relationshipFoundation418Expansion } from './relationshipFoundation418Expansion.js';
 
 export const relationshipFoundation412Expansion = Object.freeze([
   ...relationshipFoundation412ExpansionBase,
@@ -12,4 +13,5 @@ export const relationshipFoundation412Expansion = Object.freeze([
   ...relationshipFoundation415Expansion,
   ...relationshipFoundation416Expansion,
   ...relationshipFoundation417Expansion,
+  ...relationshipFoundation418Expansion,
 ]);
