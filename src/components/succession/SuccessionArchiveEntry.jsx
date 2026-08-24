7a82bab7dabf239a57ce80b14e98b24e83e0a2bf
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-08-24 22:13:14 +0300

    Load reboot lock after historical Succession styles

--- src/components/succession/SuccessionArchiveEntry.jsx
+++ src/components/succession/SuccessionArchiveEntry.jsx
@@ -24,6 +24,7 @@ import './SuccessionArchitectureVisualRevision.css';
 import './SuccessionPhase2PresentationConsistency.css';
 import './SuccessionFinalReleasePatch.css';
 import './SuccessionFinalContrastClosure.css';
+import './SuccessionVisualRebootLate.css';
 import SuccessionCommandHome from './SuccessionCommandHome';
 
 const SuccessionArchiveApp = lazy(() => import('./SuccessionArchiveApp'));
