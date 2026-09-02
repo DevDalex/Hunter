0a7b55f39d6898ebf13c6138530b74e7349fc233
DevDalex <58445340+DevDalex@users.noreply.github.com>
2026-09-02 22:16:24 +0300

    Register maintained Chapter 419 research

--- src/data/successionMaintainedChapterResearch.js
+++ src/data/successionMaintainedChapterResearch.js
@@ -70,6 +70,7 @@ import { succession414415ChapterResearch } from './succession414415Research.js';
 import { succession416ChapterResearch } from './succession416Research.js';
 import { succession417ChapterResearch } from './succession417Research.js';
 import { succession418ChapterResearch } from './succession418Research.js';
+import { succession419ChapterResearch } from './succession419Research.js';
 
 const succession412MaintainedChapterResearch = Object.freeze(succession412ChapterResearch.map((record) => Object.freeze({
   ...record,
@@ -100,7 +101,7 @@ export const maintainedSuccessionChapterResearch = Object.freeze([
   ...succession406ChapterResearch, ...succession407ChapterResearch, ...succession408ChapterResearch, ...succession409ChapterResearch,
   ...succession410ChapterResearch, ...succession411ChapterResearch, ...succession412MaintainedChapterResearch,
   ...succession413ChapterResearch, ...succession414415ChapterResearch, ...succession416ChapterResearch, ...succession417ChapterResearch,
-  ...succession418ChapterResearch,
+  ...succession418ChapterResearch, ...succession419ChapterResearch,
 ].sort((left, right) => left.number - right.number));
 
 export const maintainedSuccessionChapterNumbers = Object.freeze(maintainedSuccessionChapterResearch.map((record) => record.number));
